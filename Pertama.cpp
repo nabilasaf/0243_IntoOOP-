f78e0e2059c1fda2aca945256096ed12cce5015e
nabilasaf <nabilasafitri861@gmail.com>
2024-04-23 15:29:52 +0700

    memperbaiki output matakuliah

--- Pertama.cpp
+++ Pertama.cpp
@@ -33,8 +33,8 @@ class Matakuliah{
 
     void output(){
         cout << "Kode MK: " << kodeMk << endl;
-        cout << "Nama MK: << namaMk << endl;
-        cout << "SKS: << sks << endl;
+        cout << "Nama MK: " << namaMk << endl;
+        cout << "SKS: " << sks << endl;
     }
 
 };
